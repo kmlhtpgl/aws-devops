e7ad8b9718e07721eaee82dc22abc8d7990fb1ba
kmlhtpgl <kemaledu@gmail.com>
2021-03-31 15:58:01 +0100

    31_march

---
+++ python/hands-on/flask-04-handling-forms-run-on-ec2-linux2/app.py
@@ -0,0 +1,21 @@
+# Import Flask modules
+from flask import Flask, render_template
+
+# Create an object named app
+
+
+# Create a function named `home` which uses template file named `index.html` given under `templates` folder, send your name as template variable, and assign it root path ('/')
+
+
+# Write a function named `greet` which uses template file named `greet.html` given under `templates` folder. it takes parameters from query string on URL. If it doesn't have any parameter, warning massage is raised
+
+
+
+# Write a function named `login` which uses `GET` and `POST` methods,
+# and template files named `login.html` and `secure.html` given under `templates` folder
+# and assign to the static route of ('login'). It controls If password is clarusway or not
+
+
+# Add a statement to run the Flask application which can be reached from any host on port 80.
+
+# app.run(host='0.0.0.0', port=80)
