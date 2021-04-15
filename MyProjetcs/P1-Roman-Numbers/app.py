765c515bf4a612d8f634d7e0c70286c462bc5abf
kmlhtpgl <kemaledu@gmail.com>
2021-04-15 10:58:26 +0100

    fifth

--- MyProjetcs/P1-Roman-Numbers/app.py
+++ MyProjetcs/P1-Roman-Numbers/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app=Flask(__name__)
 
 
-@app.route('/',methods=["GET"])
+@app.route('/', methods=["GET"])
 def index():
         return render_template('index.html')
 
