a9f66316c885d96e383071375f0ff8b66a9bb1a0
kmlhtpgl <kemaledu@gmail.com>
2021-04-23 17:59:34 +0100

    23April

--- MyProjetcs/MyTeamworks/TW-6/generate-password.py
+++ MyProjetcs/MyTeamworks/TW-6/generate-password.py
@@ -1,20 +1,13 @@
 def generatepassword():
     name = input("Please enter your full name without space: ")
-    digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     password = ""
-    namelower = name.lower()
-    def split(namelower):
-        return [char for char in namelower]
-    #print(split(namelower))
     import random
-    three_letters = random.sample(split(namelower), 3)
-    #print(three_letters)
-    three_digits = random.sample(digits, 4)
-    #print(three_digits)
+    three_letters = random.sample(name, 3)
+    four_digits = random.sample(range(10), 4)
     for i in three_letters:
         password += i
-    for l in three_digits:
+    for l in four_digits:
         password += str(l)
-    print(password)  
+    print(password.lower())  
 
 generatepassword()
